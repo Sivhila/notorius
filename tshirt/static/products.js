4afbce5e4dbe2af063c96748e16698b13e073749
Sivhila <anninachilombe@gmail.com>
2026-09-08 04:10:19 -0700

    update product

---
+++ tshirt/static/products.js
@@ -0,0 +1,15 @@
+const products = [
+	    {
+		            id: "01",
+		            name: "Apparel PDF Design",
+		            price: 5.00,
+		            mainImg: "static/images/apparel.png",
+		            thumbnails: [
+				    "static/images/apparel.png",
+				    "static/images/apparel2.png",
+				    "static/images/apparel3.png",
+				    "static/images/apparel4.png"
+			    ],
+		            description: "High-resolution digital design ready to use with Cricut or Silhouette."
+	    },
+];
